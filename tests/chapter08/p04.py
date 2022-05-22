460cddeef38303297ff9ac3809c7209f2fa8f744
yassineayadi <ayadi.yas@gmail.com>
2022-05-22 14:30:20 +0200

    [DP] Add solution to power set problem

---
+++ tests/chapter08/p04.py
@@ -0,0 +1,5 @@
+from chapter08.p04 import get_all_subsets_of_sets_substring_solution
+
+def test_find_all_subsets():
+    universe = [1,2,3,4]
+    get_all_subsets_of_sets_substring_solution(universe)
