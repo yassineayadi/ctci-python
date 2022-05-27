8ef613e7b9aac0394cb455f879e381eec730d100
yasayadi <yasayadi@amazon.com>
2022-05-27 17:58:10 +0200

    [DP] Add solution to queen placing problem

--- tests/chapter08/test_p12.py
+++ tests/chapter08/test_p12.py
@@ -5,7 +5,6 @@ from chapter08.p12 import (
     is_valid_column,
     is_valid_diagonal,
 )
-from chapter08.p12_ctci import queens
 
 
 def test_place_eight_queens():
@@ -31,7 +30,3 @@ def test_is_valid_diagonal():
     grid: list[list[str]] = [["_"] * 8 for _ in range(8)]  # noqa
     grid[0][0] = "Q"
     assert is_valid_diagonal(grid, 7, 7) is False
-
-
-def test_place_eight_queens_ctci():
-    print(f"{len(queens())=}")
